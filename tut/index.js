9fb2cd921dcd5d1375cf3003d79c479494389adc
Eric Laursen <ericjlaursen@gmail.com>
2018-06-14 22:00:03 -0400

    Ignores node_mods.

--- tut/index.js
+++ tut/index.js
@@ -1,8 +1,13 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (sock) =>{
+	console.log('a user is connected!');
 })
 
 http.listen(3000, () =>{
